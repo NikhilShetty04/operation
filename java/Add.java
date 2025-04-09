5a2ddc092eb08e01bd3fa352eba280d78c5d0cd5
manju805 <m8176802@gmail.com>
2025-04-09 14:28:51 +0530

    save java master

--- java/Add.java
+++ java/Add.java
@@ -1,11 +1,13 @@
 public class Add
 {
     public static void main(String[] args) {
-        int a=10;
+        int a=30;
         int b=20;
 
         int sum=a+b;
+        int sub=a-b;
 
-        System.out.println("Reault:"+sum);
+        System.out.println("Addition:"+sum);
+        System.out.println("Subtraction:"+sub);
     }
 }
